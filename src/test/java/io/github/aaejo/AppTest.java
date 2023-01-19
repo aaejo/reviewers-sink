53e46c61035625900ea7a8a756ffa4afa21caa27
OmriHarary <omriharary@hotmail.com>
2023-01-19 14:04:00 -0500

    Initialize Java projects

---
+++ src/test/java/io/github/aaejo/AppTest.java
@@ -0,0 +1,20 @@
+package io.github.aaejo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
